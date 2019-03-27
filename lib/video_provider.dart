5201acf467324282425ba737866c34f4bcc76e79
Stefan <Stefan.Lobbenmeier@web.de>
2019-03-27 10:50:38 +0100

    Exported checked_video_provider

--- lib/video_provider.dart
+++ lib/video_provider.dart
@@ -7,3 +7,4 @@ export 'package:video_provider/src/filetype.dart';
 export 'package:video_provider/src/resolution.dart';
 export 'package:video_provider/src/video.dart';
 export 'package:video_provider/src/video_provider.dart';
+export 'package:video_provider/src/checked_video_provider.dart';
