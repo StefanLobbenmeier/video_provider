d35ab33f566ca6fdedcde49cc1f39185369055cd
Stefan <Stefan.Lobbenmeier@web.de>
2019-03-27 13:18:26 +0100

    Fixed dependency

--- test/test_all.dart
+++ test/test_all.dart
@@ -1,4 +1,5 @@
-import 'concrete_video_providers/test_all_concrete_video_providers.dart' as test_all_concrete_video_providers;
+import 'concrete_video_providers/test_all_concrete_video_providers.dart'
+as test_all_concrete_video_providers;
 import 'test_checked_video_provider.dart' as test_checked_video_provider;
 import 'test_video_provider.dart' as test_video_provider;
 
