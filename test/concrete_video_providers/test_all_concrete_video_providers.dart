c0fef8b0022d02e41e45352b244235ff22e3567e
Stefan <Stefan.Lobbenmeier@web.de>
2019-03-27 11:32:52 +0100

    Prepared for easier testing of concrete_video_providers

---
+++ test/concrete_video_providers/test_all_concrete_video_providers.dart
@@ -0,0 +1,7 @@
+import 'test_gfycat.dart' as test_gfycat_to_mp4;
+import 'test_imgur.dart' as test_imgur_to_mp4;
+
+void main() {
+  test_imgur_to_mp4.main();
+  test_gfycat_to_mp4.main();
+}
--- test/test_all.dart
+++ test/test_all.dart
@@ -1,14 +1,12 @@
-import 'concrete_video_providers/test_imgur.dart' as test_imgur_to_mp4;
-import 'concrete_video_providers/test_gfycat.dart' as test_gfycat_to_mp4;
-import 'test_video_provider.dart' as test_video_provider;
+import 'concrete_video_providers/test_all_concrete_video_providers.dart' as test_all_concrete_video_providers;
 import 'test_checked_video_provider.dart' as test_checked_video_provider;
+import 'test_video_provider.dart' as test_video_provider;
 
 void main() {
   // Online
   test_checked_video_provider.main();
 
   // Offline
-  test_imgur_to_mp4.main();
-  test_gfycat_to_mp4.main();
   test_video_provider.main();
+  test_all_concrete_video_providers.main();
 }
