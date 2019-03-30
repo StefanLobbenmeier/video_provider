94f23f3115093ff19fd0677aedbfc3d833aba5b9
Stefan Lobbenmeier <36509607+StefanLobbenmeier@users.noreply.github.com>
2019-03-30 17:50:29 +0100

    Bug fix in imgur

--- lib/src/concrete_video_providers/imgur.dart
+++ lib/src/concrete_video_providers/imgur.dart
@@ -15,7 +15,6 @@ class ImgurProvider extends VideoProvider {
 }
 
 Uri imgur_to_mp4(Uri gifv_url) {
-  assert(gifv_url.path.endsWith(".gifv"));
   String newPath = gifv_url.path.replaceRange(
       gifv_url.path.lastIndexOf("."), gifv_url.path.length, ".mp4");
   return gifv_url.replace(path: newPath);
