87da493eac0f823a735ea144d309763e31a12179
Stefan <Stefan.Lobbenmeier@web.de>
2019-05-29 17:42:01 +0200

    Added toString, == and hashCode to Beans

--- lib/src/resolution.dart
+++ lib/src/resolution.dart
@@ -4,5 +4,21 @@ class Resolution {
   static Resolution normal = Resolution(_levelIncrementor++);
   static Resolution high = Resolution(_levelIncrementor++);
 
-  Resolution(int i);
+  final int level;
+  const Resolution(this.level);
+
+  @override
+  String toString() {
+    return 'Resolution{level: $level}';
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+          other is Resolution &&
+              runtimeType == other.runtimeType &&
+              level == other.level;
+
+  @override
+  int get hashCode => level.hashCode;
 }
--- lib/src/video.dart
+++ lib/src/video.dart
@@ -12,4 +12,24 @@ class Video {
     this.filetype,
     this.uri,
   );
+
+  @override
+  String toString() {
+    return 'Video{resolution: $resolution, filetype: $filetype, uri: $uri}';
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+          other is Video &&
+              runtimeType == other.runtimeType &&
+              resolution == other.resolution &&
+              filetype == other.filetype &&
+              uri == other.uri;
+
+  @override
+  int get hashCode =>
+      resolution.hashCode ^
+      filetype.hashCode ^
+      uri.hashCode;
 }
